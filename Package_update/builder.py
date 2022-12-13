c98743243a79cb0f33e97b25a08d64805b5fd4de
hmartinez <hmartinez@abae.gob.ve>
2022-12-12 20:08:28 -0400

    Version estable 20221212 2008

--- Package_update/builder.py
+++ Package_update/builder.py
@@ -1,23 +1,36 @@
-import os
-import subprocess
 import json
-from General_Utilities.cntrl_exit import salida
-from General_Utilities.option_list import option_list
-from Pku_module.Package_update_module import project_route, listar_paquetes, actualizar_setup, actualizar_paquetes
+import os
 
+# Actualizando Json
 
-dir_dist = 'dist'
+package_name = input('Introduzca el nombre del nuevo paquete: ')
+package_description = input('Introduzca la desripcion del nuevo paquete: ')
+
+param = {}
+param["name"] = package_name
+param["version"] = "0.1"
+param["description"] = package_description
+param["author"] = "Hector Martinez"
+param["author_email"] = "hectoralonzomartinez00@gmail.com"
+param["url"] = ''
+param["packages"] = [
+    package_name,
+]
 
 ruta_archivo_json = 'Package_update/settings_setups.json'
 with open(ruta_archivo_json) as archivo_json:
     datos_json = json.load(archivo_json)
 
-opciones = listar_paquetes()
-project = option_list(opciones)
-package = datos_json[project]
+datos_json[package_name] = param
 
-actualizar_setup(package)
+with open(ruta_archivo_json, 'w') as archivo_json:
+    json.dump(datos_json, archivo_json, indent=4)
 
-actualizar_paquetes(dir_dist, project)
 
-salida("Package_update/builder")
+# Creando directorio del paquete
+
+os.makedirs(package_name)
+file = package_name + '/__init__.py'
+f = open(file, 'w')
+f.write("")
+f.close()
--- Package_update/setup_generator.py
+++ Package_update/setup_generator.py
@@ -1,29 +1,29 @@
 import json
+import os
 
+package_name = input('Introduzca el nombre del nuevo paquete: ')
+package_description = input('Introduzca la desripcion del nuevo paquete: ')
 
 param = {}
-param["name"] = ''
-param["version"] = ''
-param["description"] = ''
-param["author"] = ''
-param["author_email"] = ''
+param["name"] = package_name
+param["version"] = "0.1"
+param["description"] = package_description
+param["author"] = "Hector Martinez"
+param["author_email"] = "hectoralonzomartinez00@gmail.com"
 param["url"] = ''
-param["packages"] = ''
+param["packages"] = [
+    package_name,
+]
 
 
 ruta_archivo_json = 'Package_update/settings_setups.json'
 with open(ruta_archivo_json) as archivo_json:
     datos_json = json.load(archivo_json)
 
-
-datos_json['nueva'] = param
-# datos_json['nueva']["name"] = ''
-# datos_json['nueva']["version"] = ''
-# datos_json['nueva']["description"] = ''
-# datos_json['nueva']["author"] = ''
-# datos_json['nueva']["author_email"] = ''
-# datos_json['nueva']["url"] = ''
-# datos_json['nueva']["packages"] = ''
+datos_json[package_name] = param
 
 with open(ruta_archivo_json, 'w') as archivo_json:
     json.dump(datos_json, archivo_json, indent=4)
+
+
+os.makedirs(package_name)
