0f6aee2ed17ce2620c9e70b6d4254fc1a8c516ab
hmartinez00 <hectoralonzomartinez00@gmail.com>
2022-12-13 11:11:16 -0400

    Version estable 20221213 1111

--- Package_update/single_updater.py
+++ Package_update/single_updater.py
@@ -5,32 +5,27 @@ from Pku_module.Package_update_module import listar_paquetes, actualizar_setup, 
 
 
 dir_dist = 'dist'
-
 ruta_archivo_json = 'Package_update/settings_setups.json'
-with open(ruta_archivo_json) as archivo_json:
-    datos_json = json.load(archivo_json)
 
 opciones = listar_paquetes()
 project = option_list(opciones)
+
+with open(ruta_archivo_json) as archivo_json:
+    datos_json = json.load(archivo_json)
 package = datos_json[project]
+
 
 pregunta = input('Desea actualizar version del paquete? (S/N): ')
 
 if pregunta == 's' or 'S':
-    
 
-    ruta_archivo_json = 'Package_update/settings_setups.json'
-    with open(ruta_archivo_json) as archivo_json:
-        datos_json = json.load(archivo_json)
+    version = package['version']
 
-    print(
-            'Version actual: ', 
-            datos_json['Pku_module']['version']
-        )
+    print(f'Version actual: {version}')
 
     version = input('Introduzca la nueva version: ')
 
-    datos_json['Pku_module']['version'] = version
+    package['version'] = version
 
     with open(ruta_archivo_json, 'w') as archivo_json:
         json.dump(datos_json, archivo_json, indent=4)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Pku_module',
-    version='0.1',
+    version='0.3',
     description='Utilidades para actualizacion de modulos y paquetes.',
     author='Hector Martinez',
     author_email='hectoralonzomartinez00@gmail.com',
