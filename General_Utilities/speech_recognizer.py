67373c3c296e62986d52762e3343bab7ffa86710
hmartinez00 <hectoralonzomartinez00@gmail.com>
2022-12-30 11:59:34 -0400

    Version Estable 20221230 115933

--- General_Utilities/speech_recognizer.py
+++ General_Utilities/speech_recognizer.py
@@ -1,14 +1,16 @@
-# import webbrowser
+# Puede requerir previamente pipwin
+# y pipwin install pyaudio
 import speech_recognition as sr
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print('Hola soy tu asistente de voz: ')
-    audio = r.listen(source)
+def Reconocimiento():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Te escucho: ')
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
 
-try:
-    text = r.recognize_google(audio)
-    print(f'Haz dicho: {text}')
-    print(text)
-except:
-    print('No te he entendido')
+    try:
+        text = r.recognize_google(audio, language="es-ES")
+        print(f'Haz dicho: {text}')
+    except:
+        print('No te he entendido')
