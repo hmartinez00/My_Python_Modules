9d7e29ea6a09d98f66d46980e1db1fa26a62a21d
hmartinez <hmartinez@abae.gob.ve>
2022-12-31 14:25:13 -0400

    Version Estable 20221231 142512

---
+++ Eliezer/Command_builder.py
@@ -0,0 +1,25 @@
+import json
+
+ruta_archivo_json = 'voice_comand_settings.json'
+
+datos_json = \
+{
+    "voice_optiones":{
+        "close":[
+            "finalizar dictado",
+            "finalizar comunicación",
+            "cerrar comunicación",
+            "cierra comunicación"
+        ],
+        "secuence":[
+            "nueva línea",
+            "nuevo párrafo"
+        ],
+        "clear":[
+            "borrar todo"
+        ]
+    }
+}
+
+with open(ruta_archivo_json, 'w', encoding='utf8') as archivo_json:
+    json.dump(datos_json, archivo_json, indent=4)
