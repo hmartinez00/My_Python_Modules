b5734326cd4d6ed3c7ad5dcb14885e447dc57c26
hmartinez <hmartinez@abae.gob.ve>
2023-01-02 14:34:26 -0400

    Version Estable 20230102 143425

--- menu.py
+++ menu.py
@@ -1,29 +1,32 @@
-import os
-import subprocess
-from General_Utilities.option_list import option_list
+from General_Utilities.menu import menu
 
-subprocess.run('cls', shell=True)
+menu()
 
-opciones=[
-	'Crear paquete vacio',
-	'Actualizar un paquete',
-	'Hacer commit automatico',
-	'Actualizar todos los paquetes',
-	'Salir',
-]
+# import subprocess
+# from General_Utilities.option_list import option_list
 
-opcion = option_list(opciones)
+# subprocess.run('cls', shell=True)
 
-if opcion==opciones[0]:
-	exec(open("Package_update/builder.py").read())
-elif opcion==opciones[1]:
-	exec(open("Package_update/single_updater.py").read())
-elif opcion==opciones[2]:
-	exec(open("Package_update/git_update.py").read())
-elif opcion==opciones[3]:
-	exec(open("Package_update/full_updater.py").read())
-elif opcion==opciones[4]:
-	print('Adios!')
-else:
-	print('\nOpcion Invalida! Repita la eleccion.\n')
-	exec(open("menu.py").read())
+# opciones=[
+# 	'Crear paquete vacio',
+# 	'Actualizar un paquete',
+# 	'Hacer commit automatico',
+# 	'Actualizar todos los paquetes',
+# 	'Salir',
+# ]
+
+# opcion = option_list(opciones)
+
+# if opcion==opciones[0]:
+# 	exec(open("Package_update/builder.py").read())
+# elif opcion==opciones[1]:
+# 	exec(open("Package_update/single_updater.py").read())
+# elif opcion==opciones[2]:
+# 	exec(open("Package_update/git_update.py").read())
+# elif opcion==opciones[3]:
+# 	exec(open("Package_update/full_updater.py").read())
+# elif opcion==opciones[4]:
+# 	print('Adios!')
+# else:
+# 	print('\nOpcion Invalida! Repita la eleccion.\n')
+# 	exec(open("menu.py").read())
