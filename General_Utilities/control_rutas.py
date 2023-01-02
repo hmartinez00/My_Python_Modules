cec2fe71398a585fe4585d224e3a53064450181e
hmartinez <hmartinez@abae.gob.ve>
2023-01-01 20:01:17 -0400

    Version Estable 20230101 200116

--- General_Utilities/control_rutas.py
+++ General_Utilities/control_rutas.py
@@ -1,20 +1,13 @@
 import json
 
 
-def setting_routes(
-    key,
-    prefix: str = None,
-    sufix: str = None,
-):
+def setting_routes(key):
     file_routes = 'settings/routes/routes.json'
 
     with open(file_routes) as archivo_json:
         datos_json = json.load(archivo_json)
     ruta_archivo = []
-    
-    prefix = 'settings/' + key + '/' if prefix is None else prefix
-
     for i in datos_json[key]:
-        ruta_archivo.append([i, prefix + i + sufix])
+        ruta_archivo.append(i)
     
     return ruta_archivo
