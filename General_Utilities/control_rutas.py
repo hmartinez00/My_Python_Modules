8b6dfb8baf13db736c4aeccf553241d4b8e6b3f8
hmartinez <hmartinez@abae.gob.ve>
2023-02-13 21:43:47 -0400

    Version Estable 20230213 214346

--- General_Utilities/control_rutas.py
+++ General_Utilities/control_rutas.py
@@ -6,8 +6,12 @@ def setting_routes(key):
 
     with open(file_routes) as archivo_json:
         datos_json = json.load(archivo_json)
-    ruta_archivo = []
-    for i in datos_json[key]:
-        ruta_archivo.append(i)
     
-    return ruta_archivo
+    if key != 'sub_exec':
+        ruta_archivo = []
+        for i in datos_json[key]:
+            ruta_archivo.append(i)
+        
+        return ruta_archivo
+    elif key == 'sub_exec':
+        return datos_json[key]
