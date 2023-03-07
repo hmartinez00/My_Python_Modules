744d760db58846c3895e7a273861d35772658669
hmartinez <hmartinez@abae.gob.ve>
2023-03-07 19:12:05 -0400

    Version Estable 20230307 191202

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='MessagesKit',
-    version='0.2',
-    description='Utilidades para mensajeria.',
+    name='V2Gen',
+    version='0.1',
+    description='Paquete ligero de modulos para generacion VRSS',
     author='Hector Martinez',
     author_email='hectoralonzomartinez00@gmail.com',
     url='',
-    packages=['MessagesKit']
+    packages=['V2Gen']
 )
