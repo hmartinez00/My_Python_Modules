a176d0e390894b2d5552ca549e1805b36d5e28c8
hmartinez <hmartinez@abae.gob.ve>
2023-06-21 16:29:54 -0400

    Version Estable 20230621 162949

--- General_Utilities/prettify.py
+++ General_Utilities/prettify.py
@@ -4,6 +4,13 @@ from xml.etree import ElementTree
 from xml.etree.ElementTree import Element, SubElement, Comment
 from xml.dom import minidom
 
+import unicodedata
+
+def remove_accents(value):
+    normalized_value = unicodedata.normalize('NFKD', value)
+    ascii_value = normalized_value.encode('ASCII', 'ignore').decode('utf-8')
+    return ascii_value
+
 
 def prettify(__elem__):
     """Return a pretty-printed XML string for the Element.
