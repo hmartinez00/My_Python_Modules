3ab7085a4cc155f47f6d0aa5eb0f955979c81dd1
hmartinez <hmartinez@abae.gob.ve>
2023-08-03 16:12:14 -0400

    Version Estable 20230803 161212

--- General_Utilities/sys_clean.py
+++ General_Utilities/sys_clean.py
@@ -1,4 +1,6 @@
+import os
 import shutil
+import ctypes
 
 
 def sys_clean():
@@ -7,6 +9,8 @@ def sys_clean():
         'C:/Windows/Prefetch',
         'C:/Windows/Temp',
         'C:/Users/admin/AppData/Local/Temp',
+        'C:/Users/admin/AppData/Roaming/Code/Cache',
+        'C:/Users/admin/AppData/Roaming/Code/CachedData',
     ]
 
     def borrar_contenido_directorio(ruta_directorio):
@@ -23,7 +27,7 @@ def sys_clean():
                     elif os.path.isdir(ruta_archivo):
                         shutil.rmtree(ruta_archivo)                
                 except Exception as e:
-                    pass
+                    print(e)
         else:
             print("La ruta especificada no es un directorio válido.")   
         num1 = len(os.listdir(ruta_directorio))
@@ -34,3 +38,21 @@ def sys_clean():
     for i in dir:
         numr = borrar_contenido_directorio(i)
         print(f'Remociones de {i}: {numr}')
+    
+    input('Presione un tecla para continuar: ')
+
+
+def empty_recycle_bin():
+    try:
+        # Obtener la ruta de la papelera de reciclaje en Windows
+        recycle_bin = ctypes.c_wchar_p(os.path.expanduser("~\RecycleBin"))
+         # Eliminar todos los archivos de la papelera de reciclaje
+        for root, _, files in os.walk(recycle_bin.value):
+            for file in files:
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+        print("La papelera de reciclaje se ha vaciado correctamente.")
+    except Exception as e:
+        print("Ocurrió un error al vaciar la papelera de reciclaje:", str(e))
+
+
